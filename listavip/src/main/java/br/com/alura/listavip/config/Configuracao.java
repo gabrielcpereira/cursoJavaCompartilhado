5f046e5b8a1d49a1bc0611ddffb04b7476189913
gabriel.pereira <gabriel.pereira@yahoo.com.br>
2018-01-23 17:27:52 -0200

    Projeto SpringBoot

---
+++ listavip/src/main/java/br/com/alura/listavip/config/Configuracao.java
@@ -0,0 +1,14 @@
+package br.com.alura.listavip.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class Configuracao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+}
