97e477e5b5d3c0e6be35cd00887902ff99e042b6
gabriel.pereira <gabriel.pereira@yahoo.com.br>
2018-01-26 17:23:22 -0200

    teste

--- lista/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ lista/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -3,10 +3,8 @@ package br.com.alura.listavip;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +15,15 @@ import br.com.alura.listavip.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
-	
+		
 	@Autowired
 	private ConvidadoRepository repository;
+		
 	@Autowired
 	private HttpServletRequest request;
 	
-	/*actions*/
 	private final String LISTA_CONVIDADOS = "listaconvidados";
-	private final String LISTA_CONVIDADOS_BUSCA = "listaconvidadosbusca";
+	private final String EXCLUIR_CONVIDADO = "excluirconvidado";
 	private final String SALVAR = "salvar";
 	
 	@RequestMapping("/")
@@ -33,17 +31,18 @@ public class ConvidadoController {
 		return "index";
 	}
 	
-	@RequestMapping(value = LISTA_CONVIDADOS_BUSCA, method = RequestMethod.GET)
-	public String listaConvidadosBusca(@RequestParam(name="nomeBusca") String nomeBusca, Model model) {		
-		Iterable<Convidado> convidados = repository.findByNome(nomeBusca);
-		model.addAttribute("convidados", convidados);
-		
-		return LISTA_CONVIDADOS;
-	}
-	
 	@RequestMapping(value = LISTA_CONVIDADOS, method = RequestMethod.GET)
 	public String listaConvidados(Model model) {
-		Iterable<Convidado> convidados = repository.findAll();
+		Iterable<Convidado> convidados;
+		
+		String nomeBusca = request.getParameter("nomeBusca");
+		if (nomeBusca != null & nomeBusca != "") {
+			convidados = repository.findByNomeStartingWith(nomeBusca);
+			model.addAttribute("nomeBusca", nomeBusca);
+		}else {
+			convidados = repository.findAll();
+		}
+		
 		model.addAttribute("convidados", convidados);
 		
 		return LISTA_CONVIDADOS;
@@ -54,6 +53,13 @@ public class ConvidadoController {
 			Model model) {		
 		Convidado novoConvidado = new Convidado(nome, email, telefone);
 		repository.save(novoConvidado);
+		
+		return new ModelAndView("redirect:/" + LISTA_CONVIDADOS);
+	}
+	
+	@RequestMapping(value = EXCLUIR_CONVIDADO, method = RequestMethod.POST)
+	public ModelAndView excluirConvidado(@RequestParam(name="id") long id) {		
+		repository.delete(id);
 		
 		return new ModelAndView("redirect:/" + LISTA_CONVIDADOS);
 	}
--- lista/src/main/java/br/com/alura/listavip/repository/ConvidadoRepository.java
+++ lista/src/main/java/br/com/alura/listavip/repository/ConvidadoRepository.java
@@ -3,9 +3,13 @@ package br.com.alura.listavip.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.listavip.model.Convidado;
 
+@Repository
 public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
 	List<Convidado> findByNome(String nome);
+	List<Convidado> findByNomeEndingWith(String nome);
+	List<Convidado> findByNomeStartingWith(String nome);
 }
